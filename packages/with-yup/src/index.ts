5ea6fd42e91b61ff195bd4c1d0becdf5356e7cbd
clemvnt <clemvnt@gmail.com>
2022-08-27 03:45:21 +0200

    feat: add zod and yup validators

--- packages/with-yup/src/index.ts
+++ packages/with-yup/src/index.ts
@@ -1,1 +1,53 @@
-export const A = 1
+import { GenericValidator, ValidationErrors } from 'solid-start-enhanced-form'
+import {
+  object,
+  ObjectShape,
+  InferType,
+  ObjectSchema,
+  TypeFromShape,
+  AnyObject,
+  ValidationError
+} from 'yup'
+
+function toValidationErrors(error: ValidationError): ValidationErrors {
+  const errors: ValidationErrors = {}
+
+  for (const innerError of error.inner) {
+    if (innerError.path) {
+      errors[innerError.path] = innerError.message
+    }
+  }
+
+  return errors
+}
+
+export function withYup<T extends ObjectShape>(
+  shape: T
+): GenericValidator<InferType<ObjectSchema<TypeFromShape<T, AnyObject>>>> {
+  const schema = object(shape)
+
+  return {
+    hasValidator(name) {
+      return !!shape[name]
+    },
+    validate(data) {
+      try {
+        const result = schema.validateSync(data)
+        return { success: true, data: result }
+      } catch (error) {
+        return {
+          success: false,
+          errors: toValidationErrors(error as ValidationError)
+        }
+      }
+    },
+    validateField(name, data) {
+      try {
+        schema.validateSyncAt(name, data)
+        return undefined
+      } catch (error) {
+        return (error as ValidationError).message
+      }
+    }
+  }
+}
--- packages/with-zod/src/index.ts
+++ packages/with-zod/src/index.ts
@@ -1,1 +1,43 @@
-export const A = 1
+import { GenericValidator, ValidationErrors } from 'solid-start-enhanced-form'
+import { z, ZodError } from 'zod'
+
+function toValidationErrors(error: ZodError): ValidationErrors {
+  const errors: ValidationErrors = {}
+
+  const { fieldErrors } = error.flatten()
+
+  for (const key in fieldErrors) {
+    const fieldError = fieldErrors[key]
+    if (fieldError) {
+      errors[key] = fieldError.join(' `')
+    }
+  }
+
+  return errors
+}
+
+export function withZod<T extends z.ZodRawShape>(
+  shape: T
+): GenericValidator<z.infer<z.ZodObject<T>>> {
+  const schema = z.object(shape)
+
+  return {
+    hasValidator(name) {
+      return !!shape[name]
+    },
+    validate(data) {
+      const result = schema.safeParse(data)
+
+      return result.success
+        ? result
+        : { success: false, errors: toValidationErrors(result.error) }
+    },
+    validateField(name, data) {
+      const result = schema
+        .pick({ [name]: true } as Record<keyof T, true>)
+        .safeParse(data)
+
+      return result.success ? undefined : result.error.errors[0].message
+    }
+  }
+}
